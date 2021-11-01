2c83c20b13e0eb9e7ef1dcbaa5f5ca27911a41f4
Derek Taylor <derek@distrotube.com>
2021-11-01 09:57:57 -0500

    Fixing error in path to colorsheme.

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@
 
 static int topbar = 1;       /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;     /* -c option; centers dmenu on screen */
-static int min_width = 500;  /* minimum width when centered */
+static int min_width = 600;  /* minimum width when centered */
 static int fuzzy = 1;        /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -31,7 +31,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
  * 8. solarized-light.h
  * 9. tomorrow-night.h
  */
-#include "/home/dt/gitlab-repos/dmenu-distrotube/colors/doom-one.h"
+#include "/opt/dmenu-distrotube/colors/doom-one.h"
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
