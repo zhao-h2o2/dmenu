0508ac9b4bac670c926c6d3b04ff99ec63329050
Wang Zhaohui <18811365389@163.com>
2022-02-20 17:14:25 +0800

    fonts

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static int min_width = 600;  /* minimum width when centered */
 static int fuzzy = 1;        /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:pixelsize=11:antialias=true:autohint=true",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+	"JetBrains Nerd Font Mono:pixelsize=14:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
